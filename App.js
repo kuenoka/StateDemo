0cd0233983a7219953dfee3d7b75760acf301f6f
Kingsley Eoka <kingsleyenoka@gmail.com>
2020-09-03 12:36:26 -0500

    Saved

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState }  from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  //TODO FIX IT
+  //let counter = 0;
+  const [counter, setCounter] = useState(0);
+  const [myRandomNumber, setSelected] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button style = {styles.increaseStyle} title = 'Increase' onPress = { ()=> {
+        setCounter(counter + 1);
+        console.log(counter);
+      }}/>
+      <Button style = {styles.decreaseStyle} title = 'Decrease' onPress = { ()=> {
+        setCounter(counter - 1);
+        console.log(counter);
+      }}/>
+      <Text>Current Count {counter}</Text>
+      <Button title = 'Get Random Number' onPress = { ()=> {
+        setSelected(Math.floor(Math.random() * 10))
+      }}/>
+      <Text>Random Number {myRandomNumber}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +34,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  increaseStyle: {
+    backgroundColor: '#00ff00'
+  },
+  decreaseStyle: {
+    backgroundColor: '#ff0000'
+  }
 });
